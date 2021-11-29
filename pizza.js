57720f6210a37cabe9d889829a3a7963c1764bbc
Petra696 <91752761+Petra696@users.noreply.github.com>
2021-11-29 20:14:47 +0100

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,27 @@
+console.log("Making pizza");
+
+//writing a function for pizza
+const makepizza = function () {
+    console.log("Maak deeg");
+    console.log("Laat deeg rijzen");
+    console.log("Rol het deeg met deegroller");
+    console.log("Beleg de pizza");
+    console.log("Doe pizza in de oven");
+};
+//This code does not call in node because you must call the function
+// Calling the function
+makepizza();
+makepizza();
+
+const makesushi = function () {
+    console.log("Maak maak rijst");
+    console.log("Leg rijst op sushivel");
+    console.log("Voeg vis toe");
+    console.log("Rol de sushi");
+};
+
+makesushi();
+makesushi();
+makesushi();
+
+
